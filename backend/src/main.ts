c166d3d3864a5141ffef9a6871defe103230ecbf
Yan Rodrigues <yan.rodriguescs@outlook.com>
2024-11-15 14:48:42 -0300

    main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,7 +1,20 @@
-function greetings(message: string): void {
-	console.log(message);
-}
+import { TaskRepositoryPrisma } from "./infra/repositories/task/task.repository.prisma";
+import { prisma } from "./common/prisma/prisma";
+import { CreateTaskCase } from "./cases/create-task";
+import { ListTaskCase } from "./cases/list-tasks";
+import { CreateTaskRoute } from "./infra/http/express/routes/task/create-task";
+import { ListTasksRoute } from "./infra/http/express/routes/task/list-task";
+import { HttpExpress } from "./infra/http/express/http.express";
 
-const message = "Hello, World!";
+(() => {
+	const taskRepository = TaskRepositoryPrisma.create(prisma);
+	const createTaskCase = CreateTaskCase.create(taskRepository);
+	const listTaskCase = ListTaskCase.create(taskRepository);
 
-greetings(message);
+	const createRoute = CreateTaskRoute.create(createTaskCase);
+	const ListRoute = ListTasksRoute.create(listTaskCase);
+
+	const server = HttpExpress.create([createRoute, ListRoute]);
+
+	server.start(8080);
+})();
