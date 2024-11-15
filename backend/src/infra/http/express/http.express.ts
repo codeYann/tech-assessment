3e7e065893c0f4a8cc8d285d2a82f4cab35e38d1
Yan Rodrigues <yan.rodriguescs@outlook.com>
2024-11-15 14:46:56 -0300

    feat(backend/src/infra/http/express/http.express.ts): create HttpExpress class
    creating HtppExpress class that add routes and start the application. HttpExpress implmenents IHttp interface.

---
+++ backend/src/infra/http/express/http.express.ts
@@ -0,0 +1,32 @@
+import type { IHttp } from "../http";
+import express, { type Express } from "express";
+import type { Route } from "./routes/routes";
+
+export class HttpExpress implements IHttp {
+	private app: Express;
+
+	private constructor(routes: Route[]) {
+		this.app = express();
+		this.app.use(express.json());
+		this.addRoutes(routes);
+	}
+
+	public static create(routes: Route[]) {
+		return new HttpExpress(routes);
+	}
+
+	public start(port: number): void {
+		this.app.listen(port, () => {
+			console.log(`Aplicação rodando em localhost:${port}`);
+		});
+	}
+
+	private addRoutes(routes: Route[]): void {
+		for (const route of routes) {
+			const path = route.getPath();
+			const method = route.getMethod();
+			const handler = route.getHandler();
+			this.app[method](path, handler);
+		}
+	}
+}
