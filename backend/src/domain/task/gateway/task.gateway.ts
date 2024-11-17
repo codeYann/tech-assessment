e79cfdf2db558fdbd056ed2437369b6bbe172009
Yan Rodrigues <yan.rodriguescs@outlook.com>
2024-11-17 09:57:01 -0300

    feat(backend/src/domain/task/gateway/task.gateway.ts): add completeTasks method
    adding completeTasks method into TaskGateway

--- backend/src/domain/task/gateway/task.gateway.ts
+++ backend/src/domain/task/gateway/task.gateway.ts
@@ -3,4 +3,5 @@ import type { Task } from "../entity/task";
 export interface TaskGateway {
 	save(task: Task): Promise<void>;
 	list(): Promise<Task[]>;
+	completeTasks(tasks: Task[]): Promise<Task[]>;
 }
