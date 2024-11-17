91ed0e214d87e31f93153bb274f09548b90cae40
Yan Rodrigues <yan.rodriguescs@outlook.com>
2024-11-17 09:52:08 -0300

    style(backend/src/infra/http/express/controller/task/create-task.controller.ts): format code
    formatting code

--- backend/src/infra/http/express/controllers/task/create-task.controller.ts
+++ backend/src/infra/http/express/controllers/task/create-task.controller.ts
@@ -29,7 +29,7 @@ export class CreateTaskController {
 				await this.createTaskService.execute(input);
 
 			const responsePresenter = {
-				id: output.id,	
+				id: output.id,
 				createdAt: output.createdAt,
 			};
 
