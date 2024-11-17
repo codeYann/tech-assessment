25e02a98b54b1abd45f8002b5cce334a1e2bc476
Yan Rodrigues <yan.rodriguescs@outlook.com>
2024-11-17 10:00:13 -0300

    feat(backend/src/infra/repositories/task/task.repository.prisma.ts): implement completeTasks method
    implementing completeTasks method. completeTasks iterates over a Task list and toogles completed field, then it updates all tasks.

--- backend/src/infra/repositories/task/task.repository.prisma.ts
+++ backend/src/infra/repositories/task/task.repository.prisma.ts
@@ -21,18 +21,59 @@ export class TaskRepositoryPrisma implements TaskGateway {
 		try {
 			await this.prismaClient.task.create({ data });
 		} catch (error) {
-			console.error("Error saving task:", error);
-			throw new Error("Could not save task");
+			console.error("Erro ao criar a tarefa:", error);
+			throw new Error("Não foi possível criar a tarefa!");
 		}
 	}
 
 	public async list(): Promise<Task[]> {
 		try {
-			const tasks = await this.prismaClient.task.findMany();
+			const tasks = await this.prismaClient.task.findMany({
+				where: {
+					completed: false,
+				},
+			});
 			return tasks?.map((task) => Task.with(task)) || [];
 		} catch (error) {
-			console.error("Error fetching tasks:", error);
+			console.error("Erro ao buscar as tarefas:", error);
 			return [];
+		}
+	}
+
+	public async completeTasks(tasks: Task[]): Promise<Task[]> {
+		const completedTasks: Array<Task> = [];
+
+		for (const task of tasks) {
+			if (task.isCompleted()) {
+				throw new Error("Não é permitido completar uma tarefa já completada!");
+			}
+
+			task.toggleCompletionStatus();
+
+			completedTasks.push(
+				Task.with({
+					id: task.getId(),
+					title: task.getTitle(),
+					priority: task.getPriority(),
+					completed: task.isCompleted(),
+					createdAt: task.getCreatedAt(),
+				}),
+			);
+		}
+
+		try {
+			await Promise.all(
+				completedTasks.map((task) =>
+					this.prismaClient.task.update({
+						where: { id: task.getId() },
+						data: { completed: task.isCompleted() },
+					}),
+				),
+			);
+			return completedTasks;
+		} catch (error) {
+			console.error("Erro ao completar as tarefas:", error);
+			throw new Error("Não foi possível completar as tarefas");
 		}
 	}
 }
