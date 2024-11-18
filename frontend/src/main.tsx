802175435bdd9c0fcff72d321f3d1e05fbb09a88
Yan Rodrigues <yan.rodriguescs@outlook.com>
2024-11-18 18:44:54 -0300

    feat(frontend/src/main.tsx): add react router
    adding react router and pages

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,23 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Home } from "./pages/Home/index.tsx";
+import "./normalize.css";
+import { TasksList } from "./pages/TaskList/index.tsx";
 
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <Home />,
+	},
+	{
+		path: "/tasks",
+		element: <TasksList />,
+	},
+]);
 // biome-ignore lint/style/noNonNullAssertion: <explanation>
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
-		<App />
+		<RouterProvider router={router} />
 	</StrictMode>,
 );
